9cac46fbb5b6aba9a3095d699bf473b2ce3c07e7
suhanic44 <suhanic44@gmail.com>
2023-05-21 16:15:52 -0700

    Create Customer randomizer methods and integrate Customer and Order classes * Create OrderNode.h, which contians an OrderNode struct used by the Customer and Order class * Create generateOrders(), which randomly generates an order for each person in a group   and adds these orders to the ordersList vector in the Order class * Update Order class to use OrderStruct * Add a printing method to print all the orders from a customer group * Change Customer Class to take in a list or customer names

---
+++ header/Order.h
@@ -0,0 +1,25 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include "../header/Customer.h"
+#include "../header/OrderNode.h"
+#include <iostream>
+#include <list>
+#include<vector>
+using namespace std;
+
+class Order {
+    public:
+        void removeOrder();
+        void removeAllOrders();
+        void viewAllOrders();
+        vector<OrderNode>& getOrdersList();
+        void printOrders();
+
+    private:
+        vector<OrderNode> ordersList;
+        int numMenuItems;
+        
+};
+
+#endif
