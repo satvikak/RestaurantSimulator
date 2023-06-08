079b5717271a8ba6084ed928fddb7bbd52f50a91
suhanihb <117537786+suhanihb@users.noreply.github.com>
2023-06-07 17:19:59 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 
 int main() {
-    cout << endl << "Welcome to your new restaurant! 🍽️" << endl;
-    cout << "Enter 'QUIT' at any time to end the game." << endl << endl;
+    cout << endl << "Welcome to your new restaurant! 🍽️" << endl << endl;
 
     Restaurant r = Restaurant();
     r.simulateRestaurant();
